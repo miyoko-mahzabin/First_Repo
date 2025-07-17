435a6c278ac22830499789ca18491b34d016fdb7
Miyoko Mahzabin Mim <miyokomahzabin29@gmail.com>
2025-07-17 09:49:57 +0600

    Add files via upload

---
+++ frist.py
@@ -0,0 +1,22 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+
+
+car1 = "Ford"
+car2 = "Volvo"
+car3 = "BMW"
+
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+def my_function():
+  print("Hello from a function")
